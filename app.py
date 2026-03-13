60b1d60c035e7ab898766b9fdc81b85d423e5057
tkluwe26 <till.k0845@gmail.com>
2026-03-13 14:10:11 +0100

    Update app.py

--- app.py
+++ app.py
@@ -132,7 +132,7 @@ if not st.session_state.user_logged_in:
             st.session_state.user_logged_in=True
             st.session_state.username=username_input
 
-            st.experimental_rerun()
+            st.rerun()
 
         else:
 
